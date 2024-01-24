8a16a10538d70f4475f6d204a06ef7c307a7b06f
Lucas.Fya <Lucas_schulz@sluz.ch>
2024-01-24 15:06:05 +0100

    Die Kamera funktioniert :>

--- SoloSchnitzel/src/app/home/home.page.ts
+++ SoloSchnitzel/src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonButton,
   IonAlert,
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
 } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -16,20 +18,42 @@ import {
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonAlert],
 })
 export class HomePage {
-  public alertButtons = [
-    {
-      text: 'Cancel',
-      role: 'cancel',
-      handler: () => {
-        console.log('Alert canceled');
-      },
-    },
-    {
-      text: 'OK',
-      role: 'confirm',
-      handler: () => {
-        console.log('Alert confirmed');
-      },
-    },
-  ];
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'HALLO',
+      cssClass: 'custom-alert',
+      buttons: [
+        {
+          text: 'ACCEPT',
+          cssClass: 'alert-button-accept',
+          handler: () => {
+            // Aktion für "Accept"
+            this.accessCamera();
+          },
+        },
+        {
+          text: 'DECLINE',
+          cssClass: 'alert-button-decline',
+          handler: () => {
+            console.log('Camera access declined');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+  async accessCamera() {
+    try {
+      const cameraPhoto = await Camera.getPhoto({
+        resultType: CameraResultType.Uri, // oder 'base64', 'dataUrl'
+        quality: 90, // Bildqualität
+      });
+      console.log(cameraPhoto);
+    } catch (e) {
+      console.error('Camera issue: ', e);
+    }
+  }
 }

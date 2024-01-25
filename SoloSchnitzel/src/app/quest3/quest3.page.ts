3f11965518aa4d14ee44ef37adc8aa641e254dcc
Lucas.Fya <Lucas_schulz@sluz.ch>
2024-01-25 14:01:08 +0100

    ionic motion handy test version

--- SoloSchnitzel/src/app/quest3/quest3.page.ts
+++ SoloSchnitzel/src/app/quest3/quest3.page.ts
@@ -1,20 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {Component, OnInit} from '@angular/core';
+import { Motion } from '@capacitor/motion';
+import { PluginListenerHandle } from '@capacitor/core';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-quest3',
   templateUrl: './quest3.page.html',
   styleUrls: ['./quest3.page.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    IonicModule
+  ], // Importieren Sie hier das IonicModule
+  standalone: true
 })
 export class Quest3Page implements OnInit {
+  motionData: string = '';
+  private motionHandler: PluginListenerHandle | null = null;
+  private intervalId: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    this.startMotionListener();
   }
 
+  async startMotionListener() {
+    this.motionHandler = await Motion.addListener('accel', event => {
+      // Hier verarbeiten Sie das Bewegungsereignis
+      this.motionData = `X: ${event.acceleration.x.toFixed(3)}, Y: ${event.acceleration.y.toFixed(3)}, Z: ${event.acceleration.z.toFixed(3)}`;
+    });
+
+    // Starten Sie das Intervall, um die Funktion alle 3 Sekunden auszuführen
+    this.intervalId = setInterval(() => {
+      if (this.motionData) {
+        console.log(this.motionData);
+        // Hier könnten Sie weitere Logik implementieren
+      }
+    }, 3000);
+  }
+
+  ngOnDestroy() {
+    // Listener entfernen, wenn die Komponente zerstört wird
+    this.motionHandler?.remove();
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }

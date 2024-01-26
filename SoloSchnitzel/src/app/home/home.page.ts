f32e1fe9fca97aba2fcb06924a6e3b60e2cd8d2a
Yves Wegner <yves_wegner@sluz.ch>
2024-01-26 11:35:45 +0100

    homepage

--- SoloSchnitzel/src/app/home/home.page.ts
+++ SoloSchnitzel/src/app/home/home.page.ts
@@ -88,6 +88,10 @@ export class HomePage {
     });
     await alert.present();
   }
+  async openLeaderboard() {
+    this.router.navigateByUrl('/leaderboard'); // Verwenden Sie den Router, um zur Seite zu navigieren
+
+  }
   async presentNamePrompt() {
     const alert = await this.alertController.create({
       header: 'Spielerinformation',
@@ -115,6 +119,7 @@ export class HomePage {
         }
       ]
     });
+
 
     await alert.present();
   }

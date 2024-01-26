747b942d3167b49e7c89fb0b1a5e29ea2884f159
Lucas.Fya <Lucas_schulz@sluz.ch>
2024-01-26 09:38:59 +0100

    ionic motion handy test version

--- SoloSchnitzel/src/app/quests-info/quests-info.page.ts
+++ SoloSchnitzel/src/app/quests-info/quests-info.page.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IonicModule} from "@ionic/angular";
 import { Router } from '@angular/router';
 import {AlertController} from "@ionic/angular/standalone";
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-quests-info',
@@ -12,18 +13,50 @@ import {AlertController} from "@ionic/angular/standalone";
     IonicModule
   ],
 })
-export class QuestsInfoPage {
-  constructor(private router: Router) {}
-  goToQuest1() {
-    this.router.navigateByUrl('/quest1');
+export class QuestsInfoPage implements OnInit{
+  //erstes true weil quest 1 von anfang an Frei ist
+  questStatus: boolean[] = [true , false, false, false];
+  private storage: Storage | null = null;
+
+  constructor(
+    private router: Router,
+    private alertController: AlertController
+  ) {}
+  async ngOnInit() {
+    await this.initStorage();
+    await this.loadQuestStatus();
   }
-  goToQuest2() {
-    this.router.navigateByUrl('/quest2');
+  async initStorage() {
+    const storage = new Storage();
+    await storage.create();
+    this.storage = storage;
   }
-  goToQuest3() {
-    this.router.navigateByUrl('/quest3');
+
+  async loadQuestStatus() {
+    const savedStatus = await this.storage?.get('questStatus');
+    if (savedStatus) {
+      this.questStatus = savedStatus;
+    } else {
+      this.questStatus = [true, false, false, false];
+    }
   }
-  goToQuest4() {
-    this.router.navigateByUrl('/quest4');
+  async checkAndNavigate(questNumber: number) {
+    await this.loadQuestStatus();
+    if (this.questStatus[questNumber - 1]) {
+      this.router.navigateByUrl(`/quest${questNumber}`);
+    } else {
+      const alert = await this.alertController.create({
+        cssClass: "Questlockedalert",
+        header: 'Quest gesperrt',
+        message: 'Diese Quest ist noch nicht freigeschaltet. Beende die vorherigen Quests, um fortzufahren.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    }
+  }
+  async completeQuest(questNumber: number) {
+    this.questStatus[questNumber - 1] = true;
+    await this.storage?.set('questStatus', this.questStatus);
   }
 }
